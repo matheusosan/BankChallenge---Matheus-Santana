424104ee3f27de661855d42c5cfeef2f312b5af7
matheusosan <matheussantanaoli@gmail.com>
2025-01-22 17:16:32 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bank Challenge
+
+### To run the project, it will be necessary to compile
+`mvn clean install`
---
+++ src/main/java/br/com/compass/App.java
@@ -0,0 +1,94 @@
+package br.com.compass;
+
+import java.util.Scanner;
+
+public class App {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        mainMenu(scanner);
+        
+        scanner.close();
+        System.out.println("Application closed");
+    }
+
+    public static void mainMenu(Scanner scanner) {
+        boolean running = true;
+
+        while (running) {
+            System.out.println("========= Main Menu =========");
+            System.out.println("|| 1. Login                ||");
+            System.out.println("|| 2. Account Opening      ||");
+            System.out.println("|| 0. Exit                 ||");
+            System.out.println("=============================");
+            System.out.print("Choose an option: ");
+
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    bankMenu(scanner);
+                    return;
+                case 2:
+                    // ToDo...
+                    System.out.println("Account Opening.");
+                    break;
+                case 0:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid option! Please try again.");
+            }
+        }
+    }
+
+    public static void bankMenu(Scanner scanner) {
+        boolean running = true;
+
+        while (running) {
+            System.out.println("========= Bank Menu =========");
+            System.out.println("|| 1. Deposit              ||");
+            System.out.println("|| 2. Withdraw             ||");
+            System.out.println("|| 3. Check Balance        ||");
+            System.out.println("|| 4. Transfer             ||");
+            System.out.println("|| 5. Bank Statement       ||");
+            System.out.println("|| 0. Exit                 ||");
+            System.out.println("=============================");
+            System.out.print("Choose an option: ");
+
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    // ToDo...
+                    System.out.println("Deposit.");
+                    break;
+                case 2:
+                    // ToDo...
+                    System.out.println("Withdraw.");
+                    break;
+                case 3:
+                    // ToDo...
+                    System.out.println("Check Balance.");
+                    break;
+                case 4:
+                    // ToDo...
+                    System.out.println("Transfer.");
+                    break;
+                case 5:
+                    // ToDo...
+                    System.out.println("Bank Statement.");
+                    break;
+                case 0:
+                    // ToDo...
+                    System.out.println("Exiting...");
+                    running = false;
+                    return;
+                default:
+                    System.out.println("Invalid option! Please try again.");
+            }
+        }
+    }
+    
+}
